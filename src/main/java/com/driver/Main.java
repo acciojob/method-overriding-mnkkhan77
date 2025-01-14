e92d6b1e2e2d566ad91024aae6bb7243bab48b79
mnkkhan <mnkkhan77@gmail.com>
2025-01-14 23:20:08 +0530

    - Assignment solved Method Overriding (added B's implementation)

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        // earlier content of A was getting printed
         B obj = new B();
         System.out.println(obj.meth());
     }
